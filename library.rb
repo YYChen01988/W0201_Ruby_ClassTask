b0c87bded586d00d38a382f77b04724ebd815c59
yingying <chen.yingying.01988@gmail.com>
2018-07-23 22:01:20 +0100

    additional functions and tests added

--- library.rb
+++ library.rb
@@ -1,12 +1,12 @@
-# Create a class for a Library that has an array of books. Each book should be a hash with a title, which is a string, and rental details, which is another hash with a student name and due date.
+## Create a class for a Library that has an array of books. Each book should be a hash with a title, which is a string, and rental details, which is another hash with a student name and due date.
 
-# Create a getter for the books
+## Create a getter for the books
 
-# Create a method that takes in a book title and returns all of the information about that book.
+## Create a method that takes in a book title and returns all of the information about that book.
 
-# Create a method that takes in a book title and returns only the rental details for that book.
+## Create a method that takes in a book title and returns only the rental details for that book.
 
-# Create a method that takes in a book title and adds it to our book list (add a new hash for the book with the student name and date being left as empty strings)
+## Create a method that takes in a book title and adds it to our book list (add a new hash for the book with the student name and date being left as empty strings)
 
 # Create a method that changes the rental details of a book by taking in the title of the book, the student renting it and the date it's due to be returned.
 
@@ -25,8 +25,38 @@ def add_book(new_book)
   @books << new_book
 end
 def get_book_by_name(book_name)
-
+  for book in @books
+    if book[:title] == book_name
+      return book
+    end
+  end
 end
 
+def check_rental_detail_by_book_name(book_name)
+  for book in @books
+    if book[:title] == book_name
+      return book[:rental_details]
+    end
+  end
+end
+
+def add_book_by_title(title)
+  book = {
+  title: title,
+  rental_details: {
+   student_name: "",
+   date: "" }
+  }
+  @books << book
+end
+
+def update_rental_detail_by_book_name(book_name, new_student_name, new_date)
+  for book in @books
+    if book[:title] == book_name
+       book[:rental_details][:student_name] = new_student_name
+       book[:rental_details][:date] = new_date
+    end
+  end
+end
 
 end
--- specs/library_spec.rb
+++ specs/library_spec.rb
@@ -12,6 +12,12 @@ class TestLibrary < MiniTest::Test
      student_name: "Jeff",
      date: "01/12/18" }
     }
+    @book_2 = {
+    title: "harry_potter",
+    rental_details: {
+     student_name: "Jeff",
+     date: "01/12/18" }
+    }
   end
 
 
@@ -25,6 +31,43 @@ class TestLibrary < MiniTest::Test
     assert_equal(1, @library.get_books().length())
   end
 
+  def test_get_book_by_name()
+    @library.add_book(@book_1)
+    @library.add_book(@book_2)
 
+    assert_equal(@book_1, @library.get_book_by_name("lord_of_the_rings"))
+    assert_equal(@book_2, @library.get_book_by_name("harry_potter"))
+  end
 
+  def test_check_rental_detail_by_book_name()
+    @library.add_book(@book_1)
+    result = @book_1[:rental_details]
+    assert_equal(result, @library.check_rental_detail_by_book_name("lord_of_the_rings"))
+  end
+
+  def test_add_book_by_title()
+    @library.add_book_by_title("Sweetcorn")
+    result = @library.get_book_by_name("Sweetcorn")
+    expected =
+    {
+      title: "Sweetcorn",
+      rental_details: {
+      student_name: "",
+      date: "" }
+     }
+    assert_equal(expected, result)
+  end
+
+  def test_update_rental_detail_by_book_name()
+    @library.add_book(@book_1)
+    @library.update_rental_detail_by_book_name("lord_of_the_rings", "Ted", "23/07/18")
+    expected =
+    {
+      title: "lord_of_the_rings",
+      rental_details: {
+      student_name: "Ted",
+      date: "23/07/18" }
+    }
+    assert_equal(expected, @library.get_book_by_name("lord_of_the_rings"))
+  end
   end
